06d13cc85b9323bd33823ab918c0a7db1dbc2a8c
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-06-22 18:12:24 +0500

    header end

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let toggleBtn = document.querySelector('.toggle');
+let elNav = document.querySelector('.header__nav');
+
+toggleBtn.addEventListener('click', () => {
+  elNav.classList.toggle('nav-open');
+  toggleBtn.classList.toggle('toggle-close');
+});
