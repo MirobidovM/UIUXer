1b421f865289215aceec5028abcc4bdf29103c01
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-06-23 23:18:45 +0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 let toggleBtn = document.querySelector('.toggle');
+
 let elNav = document.querySelector('.header__nav');
 
 toggleBtn.addEventListener('click', () => {
   elNav.classList.toggle('nav-open');
+
   toggleBtn.classList.toggle('toggle-close');
 });
